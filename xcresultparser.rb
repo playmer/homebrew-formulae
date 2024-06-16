7d1abc84d9e828cbcf906b7e47298807b02c15db
alex da franca <kiki@farbflash.de>
2024-06-16 13:16:13 +0200

    Update xcresultparser.rb
    xcresultparser version 1.6.3

--- xcresultparser.rb
+++ xcresultparser.rb
@@ -1,9 +1,9 @@
 class Xcresultparser < Formula
   desc "Parse .xcresult files and print summary in different formats"
   homepage "https://github.com/a7ex/xcresultparser"
-  version "1.6.2"
-  url "https://github.com/a7ex/xcresultparser/archive/1.6.2.tar.gz"
-  sha256 "936673bbbab678b65cb8c2e805ac2c520e4db39ad95342c3c833dc2bbb442c42"
+  version "1.6.3"
+  url "https://github.com/a7ex/xcresultparser/archive/1.6.3.tar.gz"
+  sha256 "69a5e1952a44a43cf99e4e4c8608c7b7a1d8972935c51cf66707c18ab14cdc09"
   license "MIT"
 
   depends_on xcode: ["10.0", :build]
