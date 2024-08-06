b8aab992a3842c2e79e67c5c6730c26f176827c2
Joshua T. Fisher <jofisher@microsoft.com>
2024-08-06 10:56:59 -0700

    Initial work

--- generator/gen.py
+++ generator/gen.py
@@ -7,14 +7,13 @@ def sha256sum(filename):
     with open(filename, 'rb', buffering=0) as f:
         return hashlib.file_digest(f, 'sha256').hexdigest()
 
-url = 'https://api.github.com/repos/a7ex/xcresultparser/releases'
 
-def generate_text(version, sha256):
+def generate_text(repo, version, sha256):
     return f'''class Xcresultparser@{version} < Formula
   desc "Parse .xcresult files and print summary in different formats"
-  homepage "https://github.com/a7ex/xcresultparser"
+  homepage "https://github.com/{repo}"
   version "{version}"
-  url "https://github.com/a7ex/xcresultparser/archive/{version}.tar.gz"
+  url "https://github.com/{repo}/archive/{version}.tar.gz"
   sha256 "{sha256}"
   license "MIT"
 
@@ -30,26 +29,28 @@ def generate_text(version, sha256):
 
 end'''
 
-#urllib.request.urlretrieve('https://github.com/a7ex/xcresultparser/archive/1.7.1.tar.gz', f'1.7.1.tar.gz')
-#print(sha256sum("1.7.1.tar.gz"))
-#exit()
+def generate_versions(repo):
 
+  url = f'https://api.github.com/repos/{repo}/releases'
+  response = urllib.request.urlopen(url).read()
+  response_object = json.loads(response)
 
-response = urllib.request.urlopen(url).read()
-response_object = json.loads(response)
+  for release in response_object:
+      if (release['prerelease']):
+          continue
 
-for release in response_object:
-    if (release['prerelease']):
-        continue
+      print(f'Tag: {release['tag_name']}')
+      version = release['tag_name']
+      filename = f'{version}.tar.gz'
+      file_url = f'https://github.com/{repo}/archive/{release['tag_name']}.tar.gz'
+      print(f'\tfile_url: {file_url}')
+      urllib.request.urlretrieve(file_url, filename)
+      sha256 = sha256sum(filename)
+      print(f'\tSHA256: {sha256}')
 
-    print(f'Tag: {release['tag_name']}')
-    version = release['tag_name']
-    filename = f'{version}.tar.gz'
-    file_url = f'https://github.com/a7ex/xcresultparser/archive/{release['tag_name']}.tar.gz'
-    print(f'\tfile_url: {file_url}')
-    urllib.request.urlretrieve(file_url, filename)
-    sha256 = sha256sum(filename)
-    print(f'\tSHA256: {sha256}')
+      with open(f'../xcresultparser@{version}.rb', "w") as text_file:
+          text_file.write(generate_text(version, sha256))
 
-    with open(f'../xcresultparser@{version}.rb', "w") as text_file:
-        text_file.write(generate_text(version, sha256))
+generate_versions('a7ex/findsimulator')
+generate_versions('a7ex/SBEnumerator')
+generate_versions('a7ex/xcresultparser')
